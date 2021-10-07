b30385ed8cecb01cf0e46f8a641b183adbfd57da
Andre Lino <andrerafael.lino@gmail.com>
2021-10-07 16:22:41 +0100

    Assignment 3 reviewed

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -20,6 +20,7 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
+        this.toggleModal();
         console.log("Current State is: " + JSON.stringify(values));
         alert("Current State is: " + JSON.stringify(values));
     }
